b360e34833088c1e1b10593f17357d6c80b3c3e0
Seda Nur Kılıç <75687178+sedanurkilic@users.noreply.github.com>
2020-12-23 03:53:04 +0300

    Add files via upload

---
+++ homework-2.py
@@ -0,0 +1,11 @@
+a=input("Please enter the first name:")
+b=input("Please enter the last name:")
+c=int(input("Please enter the age:"))
+d=input("Please enter the date of birth") #just year
+print(f"User's information:\n{a}\n{b}\n{c}\n{d}")
+if c < 18:
+    print("You can't go out because it's dangerous")
+else:
+    print("You can go out to the street")
+
+
